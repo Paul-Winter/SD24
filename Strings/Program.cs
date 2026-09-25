52a465818f82cb34ac5bfdab79f14c6686f622b2
Paul Winter <zimin.pavel.v@gmail.com>
2026-09-25 12:10:19 +0300

    добавлены учебные проекты

---
+++ Strings/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Strings
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1-й способ
+            string str1 = "строка в C#";
+
+            // 2-й способ
+            char[] charray = { 't', 'e', 'x', 't' };
+            string str2 = new string(charray);
+        }
+    }
+}
